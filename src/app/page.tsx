110c938a26c40b3b45688f67da564ee8609270a3
Arrastia <arrastia@pm.me>
2023-04-10 18:26:37 +0200

    fix - fallback notification trigger

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,7 @@ import { Toast } from './components/Toast';
 
 import type { Geo } from '@vercel/edge';
 
-interface HomeProps {
-  searchParams: { fallback?: boolean; location: string };
-}
+export type HomeProps = { searchParams: { fallback: string; location: string } };
 
 export default function Home({ searchParams: { fallback, location } }: HomeProps) {
   const { city, latitude, longitude }: Geo = JSON.parse(location) || {};
@@ -17,12 +15,12 @@ export default function Home({ searchParams: { fallback, location } }: HomeProps
     <main className={styles.main}>
       <section className={styles.section}>
         <div aria-hidden={true} className={styles.granular} />
-        <Map city={city} coordinates={[Number(longitude), Number(latitude)]} token={process.env.MAPBOX_TOKEN as string} />
+        <Map city={String(city)} coordinates={[Number(longitude), Number(latitude)]} token={process.env.MAPBOX_TOKEN as string} />
         <BorderMask direction="vertical" />
         <BorderMask direction="horizontal" />
       </section>
 
-      {fallback ? <Toast /> : null}
+      {JSON.parse(fallback) ? <Toast /> : null}
     </main>
   );
 }
