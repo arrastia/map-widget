e73e5bb2771e685f77bf82432fe7144f9640ae99
Arrastia <arrastia@pm.me>
2023-04-10 18:49:21 +0200

    refactor - map styles & animations

--- src/app/components/Map/Map.tsx
+++ src/app/components/Map/Map.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Fragment, useEffect, useRef, useState } from 'react';
+import { Fragment, useCallback, useEffect, useRef, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import mapBox from 'mapbox-gl';
@@ -11,55 +11,46 @@ import { Marker } from './components/Marker';
 
 import type { Map as MapType } from 'mapbox-gl';
 
-const ATARRABIA_LATITUDE = 42.83;
-const ATARRABIA_LONGITUDE = -1.61;
+const center: [number, number] = [24.753574, 59.436962];
+const style = 'mapbox://styles/mapbox/streets-v12?optimize=true';
+const ZOOM = 13;
 
-const center: [number, number] = [ATARRABIA_LONGITUDE, ATARRABIA_LATITUDE];
-const MAP_ZOOM = 14;
-const style = 'mapbox://styles/mapbox/streets-v12';
-// const style = 'mapbox://styles/mapbox/dark-v11';
-
-type MapProps = { city?: string; coordinates?: [number, number]; token: string };
+export type MapProps = { city: string; coordinates: [number, number]; token: string };
 
 export const Map = ({ city, coordinates, token }: MapProps) => {
   const [map, setMap] = useState<MapType>();
 
   const mapContainer = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    const map = new mapBox.Map({
-      accessToken: token,
-      center: [-87.637596, 41.940403],
-      container: mapContainer.current || '',
-      maxZoom: 13,
-      minZoom: 2,
-      style,
-      zoom: MAP_ZOOM
-    });
-    map.flyTo({ center: coordinates || center, zoom: MAP_ZOOM });
-
+  // prettier-ignore
+  const createMarker = useCallback((map: MapType) => {
     const element = document.createElement('div');
     const root = createRoot(element);
     root.render(<Marker />);
 
-    new mapBox.Marker(element).setLngLat(coordinates || center).addTo(map);
+    new mapBox.Marker(element).setLngLat(coordinates).addTo(map);
+  }, [coordinates]);
 
+  useEffect(() => {
+    const map = new mapBox.Map({ accessToken: token, center, container: mapContainer.current || '', minZoom: 1, maxZoom: ZOOM, style, zoom: ZOOM });
+    map.flyTo({ center: coordinates, zoom: ZOOM });
+
+    createMarker(map);
     setMap(map);
 
     return () => {
       map.remove();
     };
-  }, [coordinates, token]);
+  }, [coordinates, createMarker, token]);
 
-  const flyTo = (coordinates: [number, number]) => map?.flyTo({ center: coordinates, zoom: MAP_ZOOM });
+  const flyTo = (coordinates: [number, number]) => map?.flyTo({ center: coordinates, zoom: ZOOM });
 
   return (
     <Fragment>
       <div ref={mapContainer} />
-      <label className={styles.location} onClick={() => flyTo(coordinates || center)}>
+      <label className={styles.location} onClick={() => flyTo(coordinates)}>
         <strong>{city}</strong>
       </label>
-      <Marker />
     </Fragment>
   );
 };
