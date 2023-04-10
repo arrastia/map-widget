9ed053ab4101e051bbfcfe1e18d7572e6676ad94
Arrastia <arrastia@pm.me>
2023-04-10 19:48:01 +0200

    delete - unnecessary code

--- src/assets/icons/index.ts
+++
@@ -1,6 +0,0 @@
-import location from './location.svg';
-import noLocation from './no-location.svg';
-import zoomIn from './zoom-in.svg';
-import zoomOut from './zoom-out.svg';
-
-export { location, noLocation, zoomIn, zoomOut };
