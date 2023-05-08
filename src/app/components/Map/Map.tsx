591e761e87334b31236d2bd80c2a8fea03b2d4f3
Arrastia <arrastia@pm.me>
2023-05-08 22:07:29 +0200

    update - map component to accept different types of markers

--- src/app/components/Map/Map.tsx
+++ src/app/components/Map/Map.tsx
@@ -2,55 +2,92 @@
 import { Fragment, useCallback, useEffect, useRef, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import Image from 'next/image';
+
 import mapBox from 'mapbox-gl';
 
 import 'styles/map.css';
 import styles from './Map.module.css';
 
+import { Location } from 'app/components/Location';
 import { Marker } from 'app/components/Marker';
 
 import type { Map as MapType } from 'mapbox-gl';
 
-const center: [number, number] = [24.753574, 59.436962];
 const style = 'mapbox://styles/mapbox/streets-v12?optimize=true';
 const ZOOM = 13;
 
-export type MapProps = { city: string; coordinates: [number, number]; token: string };
+export type Location = { city: string; coordinates: [number, number] };
+export type MapProps = { lastLocation: Location; location: Location; token: string };
 
-export const Map = ({ city, coordinates, token }: MapProps) => {
+export const Map = ({ lastLocation, location, token }: MapProps) => {
   const [map, setMap] = useState<MapType>();
 
   const mapContainer = useRef<HTMLDivElement>(null);
 
-  // prettier-ignore
-  const createMarker = useCallback((map: MapType) => {
+  const createMarker = useCallback((map: MapType, coordinates: [number, number], type: 'current-location' | 'last-location') => {
     const element = document.createElement('div');
     const root = createRoot(element);
-    root.render(<Marker />);
+    root.render(<Marker type={type} />);
 
     new mapBox.Marker(element).setLngLat(coordinates).addTo(map);
-  }, [coordinates]);
+  }, []);
 
   useEffect(() => {
-    const map = new mapBox.Map({ accessToken: token, center, container: mapContainer.current || '', minZoom: 1, maxZoom: ZOOM, style, zoom: ZOOM });
-    map.flyTo({ center: coordinates, zoom: ZOOM });
+    const map = new mapBox.Map({
+      accessToken: token,
+      center: lastLocation.coordinates,
+      container: mapContainer.current || '',
+      maxZoom: ZOOM,
+      minZoom: 1,
+      style,
+      zoom: ZOOM
+    });
+    map.flyTo({ center: location.coordinates, zoom: ZOOM });
 
-    createMarker(map);
+    createMarker(map, location.coordinates, 'current-location');
+    createMarker(map, lastLocation.coordinates, 'last-location');
     setMap(map);
 
     return () => {
       map.remove();
     };
-  }, [coordinates, createMarker, token]);
+  }, [createMarker, lastLocation, location, token]);
 
   const flyTo = (coordinates: [number, number]) => map?.flyTo({ center: coordinates, zoom: ZOOM });
 
   return (
     <Fragment>
       <div ref={mapContainer} />
-      <label className={styles.location} onClick={() => flyTo(coordinates)}>
-        <strong>{city}</strong>
+      <Location />
+
+      {/* <label className={`${styles.location} ${styles.current}`} onClick={() => flyTo(location.coordinates)}>
+        Current location: <strong>{location.city}</strong>
       </label>
+      <label className={`${styles.location} ${styles.last}`} onClick={() => flyTo(lastLocation.coordinates)}>
+        Last location from: <strong>{lastLocation.city}</strong>
+      </label> */}
+      <div className={styles.controls}>
+        <Image
+          alt="Back icon"
+          className={styles.icon}
+          height={20}
+          onClick={() => flyTo(lastLocation.coordinates)}
+          role="button"
+          src="/assets/icons/previous-location.svg"
+          width={20}
+        />
+        <hr aria-hidden={true} className={styles.divider} />
+        <Image
+          alt="Location icon"
+          className={styles.icon}
+          height={20}
+          onClick={() => flyTo(location.coordinates)}
+          role="button"
+          src="/assets/icons/location.svg"
+          width={20}
+        />
+      </div>
     </Fragment>
   );
 };
