816981ce0d69a5bbe5d1088d66ccf9c161acbf38
Arrastia <arrastia@pm.me>
2023-05-08 22:15:51 +0200

    refactor - map view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,17 @@ export type HomeProps = { searchParams: { fallback: string; location: string; la
 
 export default function Home({ searchParams: { fallback, location, last_location } }: HomeProps) {
   const { city, latitude, longitude }: Geo = JSON.parse(location) || {};
+  const { city: lastCity, latitude: lastLatitude, longitude: lastLongitude }: Geo = JSON.parse(last_location) || {};
 
   return (
     <main className={styles.main}>
       <section className={styles.section}>
         <div aria-hidden={true} className={styles.granular} />
-        <Map city={decodeURIComponent(String(city))} coordinates={[Number(longitude), Number(latitude)]} token={process.env.MAPBOX_TOKEN as string} />
+        <Map
+          lastLocation={{ city: decodeURIComponent(String(city)), coordinates: [Number(longitude), Number(latitude)] }}
+          location={{ city: decodeURIComponent(String(lastCity)), coordinates: [Number(lastLongitude), Number(lastLatitude)] }}
+          token={process.env.MAPBOX_TOKEN as string}
+        />
         <BorderMask direction="vertical" />
         <BorderMask direction="horizontal" />
       </section>
--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,7 @@ import kv from '@vercel/kv';
 import type { Geo } from '@vercel/edge';
 import type { NextRequest } from 'next/server';
 
-const LAST_LOCATION_FALLBACK = { city: 'Tallinn', country: 'ESTONIA', countryRegion: 'NC', region: null, latitude: 42.8273, longitude: -1.5919 };
+const LAST_LOCATION_FALLBACK = { city: 'Tallinn', country: 'ESTONIA', countryRegion: 'NC', region: null, latitude: 59.436962, longitude: 24.753574 };
 const LOCATION_FALLBACK = { city: 'Huarte-Uharte', country: 'ES', countryRegion: 'NC', region: 'cdg1', latitude: 42.8273, longitude: -1.5919 };
 
 export const config = { matcher: '/' };
@@ -18,7 +18,7 @@ export async function middleware(request: NextRequest) {
   const { nextUrl: url } = request;
 
   url.searchParams.set('fallback', JSON.stringify(!location.city));
-  url.searchParams.set('location', JSON.stringify(location.city ? location : LOCATION_FALLBACK));
+  url.searchParams.set('location', JSON.stringify(location.city ? location : LAST_LOCATION_FALLBACK));
   url.searchParams.set('last_location', JSON.stringify(lastLocation?.city ? lastLocation : LAST_LOCATION_FALLBACK));
 
   return NextResponse.rewrite(url);
